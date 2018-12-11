730d8c0ccdaffb44a2a5b24de851929c438e242d
Ian Harris <iankerriganharris@gmail.com>
2018-12-10 18:22:24 -0800

    align tslint configs

--- client/src/results/components/ResultDetail/Result.tsx
+++ client/src/results/components/ResultDetail/Result.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react'
-import { ListGroup, ListGroupItem } from 'reactstrap'
-import { LogoSwitch } from '../../../common/components'
-import _ from 'lodash'
 import { IResult } from 'anymentionofit/results'
 
 export const Result = (props: IResult) => <pre>{JSON.stringify(props.raw, null, 2)}</pre>
